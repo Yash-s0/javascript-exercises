4eca69c1a18f99fad17b3076066cb61674849f3f
Yash Sharma <yas21606@gmail.com>
2022-06-08 11:39:07 +0530

    Error Fixed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(f) 
+{
+  fTemp = Math.round((f - 32) * (5/9) * 10 )
+  fTocelsius = fTemp /10
+  return fTocelsius
 };
 
-const ctof = function() {
-
+const ctof = function(c) 
+{
+  var cTemp = Math.round(((c * 9/5) + 32) * 10)
+  var ctoFahr = cTemp / 10;
+  return ctoFahr
 };
 
 // Do not edit below this line
