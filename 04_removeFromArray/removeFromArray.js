f70cc0ecdbeb3b65994a6cf56e0fbdf6bebc5ad9
Yash Sharma <yas21606@gmail.com>
2022-06-08 09:19:45 +0530

    Error Fixed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (...args)
+{
+    const arr_ = args[0];
+    const newArray = [];
 
+    arr_.forEach(item => {
+        if(!args.includes(item))
+        {
+            newArray.push(item);
+        }
+    });
+    return newArray;
 };
 
 // Do not edit below this line
